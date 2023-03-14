8711a6355637463c45f5602d32594361d869a074
Tiago <99106150+tiagoeo@users.noreply.github.com>
2023-03-14 15:37:53 -0300

    Primeira tela com caixa de texto

--- app/src/main/java/com/ocean/android_kotlin_participacao_ocean_13_a_15_03_2023/MainActivity.kt
+++ app/src/main/java/com/ocean/android_kotlin_participacao_ocean_13_a_15_03_2023/MainActivity.kt
@@ -3,6 +3,7 @@ package com.ocean.android_kotlin_participacao_ocean_13_a_15_03_2023
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -12,9 +13,10 @@ class MainActivity : AppCompatActivity() {
 
         val btEnviar: Button = findViewById(R.id.btEnviar)
         val tvResultado: TextView = findViewById(R.id.tvResultado)
+        val etNome: EditText = findViewById(R.id.etNome)
 
         btEnviar.setOnClickListener {
-            tvResultado.text = "Sansung Ocean!"
+            tvResultado.text = etNome.text
         }
     }
 }
