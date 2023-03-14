2240c111659f3f0f2a293677792bdca74c605520
Tiago <99106150+tiagoeo@users.noreply.github.com>
2023-03-14 15:25:31 -0300

    Primeira Tela

--- app/src/main/java/com/ocean/android_kotlin_participacao_ocean_13_a_15_03_2023/MainActivity.kt
+++ app/src/main/java/com/ocean/android_kotlin_participacao_ocean_13_a_15_03_2023/MainActivity.kt
@@ -2,10 +2,19 @@ package com.ocean.android_kotlin_participacao_ocean_13_a_15_03_2023
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btEnviar: Button = findViewById(R.id.btEnviar)
+        val tvResultado: TextView = findViewById(R.id.tvResultado)
+
+        btEnviar.setOnClickListener {
+            tvResultado.text = "Sansung Ocean!"
+        }
     }
 }
