28ed0ed9d6137f0224b8136c55e33515b80edf2f
Tiago <99106150+tiagoeo@users.noreply.github.com>
2023-03-14 15:20:11 -0300

    Initial commit

---
+++ app/src/main/java/com/ocean/android_kotlin_participacao_ocean_13_a_15_03_2023/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ocean.android_kotlin_participacao_ocean_13_a_15_03_2023
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
